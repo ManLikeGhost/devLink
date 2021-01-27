13397ba0ae36b1385e1261ae852dd0ad9b8219d2
manlikeghost <ekpoabasiama@gmail.com>
2021-01-27 22:13:24 +0100

    GET ALL profiles done and tested

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -114,6 +114,12 @@ router.post(
 // @desc     Get all profiles
 // @access   Public
 router.get( '/', async ( req, res ) => {
-    
+    try {
+		const profiles = await Profile.find().populate( 'user', [ 'name', 'avatar' ] );
+		res.json( profiles );
+	} catch ( error ) {
+		console.log( error.message );
+		res.status( 500 ).send( 'Server Error' );
+	}
 } );
 module.exports = router;
