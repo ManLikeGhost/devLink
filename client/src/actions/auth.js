d5135ac580f0f86c5d73f91303543975e9d84da0
manlikeghost <ekpoabasiama@gmail.com>
2021-11-10 00:15:07 +0100

    errors display

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import { setAlert } from './alert'
 import {
     REGISTER_SUCCESS,
     REGISTER_FAIL
@@ -22,7 +22,13 @@ export const register = ( { name, email, password } ) => async dispatch => {
             type: REGISTER_SUCCESS,
             payload: res.data
         })
-    } catch (error) {
+    } catch ( err ) {
+        const errors = err.response.data.errors
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+        }
+
         dispatch( {
             type: REGISTER_FAIL
         })
