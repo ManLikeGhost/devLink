83eb2e692caaec6add3980d4757760af024c21b5
manlikeghost <ekpoabasiama@gmail.com>
2021-11-10 15:35:16 +0100

    register and authenticate complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
-
+import { loadUser } from './actions/auth';
 import './App.css';
-
 //Redux stuff
 import { Provider } from 'react-redux';
 import store from './store';
+import setAuthToken  from './util/setAuthToken'
+
+
+//check if localstorage is empty
+if (localStorage.token) {
+	setAuthToken( localStorage.token );
+}
+
 
 const App = () => {
+	useEffect( () => {
+		store.dispatch( loadUser() );
+	}, [] );
 	return (
 		<Provider store={store}>
 			<Router>
