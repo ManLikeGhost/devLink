6ac8d5797914505a7cba427ea7eb576fdc93b6c0
manlikeghost <ekpoabasiama@gmail.com>
2021-11-14 23:20:14 +0100

    setting up profile action and type

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 import Alert from './components/layout/Alert';
 import { loadUser } from './actions/auth';
 import './App.css';
@@ -34,6 +36,7 @@ const App = () => {
 						<Switch>
 							<Route exact path='/register' component={Register} />
 							<Route exact path='/login' component={Login} />
+							<PrivateRoute exact path='/dashboard' component={Dashboard} />
 						</Switch>
 					</section>
 				</Fragment>
---
+++ client/src/reducers/profile.js
@@ -0,0 +1,20 @@
+const initialState = {
+    profile: null,
+    profiles: [],
+    repos: [],
+    loading: true,
+    error: {}
+}
+
+// export default function profile(state = initialState, action) {
+//     const { type, payload } = action;
+
+//     // switch (type) {
+//     //     case value:
+            
+//     //         break;
+    
+//     //     default:
+//     //         break;
+//     // }
+// }
