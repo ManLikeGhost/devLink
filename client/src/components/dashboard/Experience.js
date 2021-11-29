8a404787a88caf078f758f1d439d178da964e66d
manlikeghost <ekpoabasiama@gmail.com>
2021-11-29 23:40:55 +0100

    setup experience

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -1,19 +1,45 @@
-import React from 'react'
-import propTypes from 'prop-types'
-import { connect } from 'react-redux'
-
+import React, { Fragment } from 'react';
+import Moment from 'react-moment';
+import propTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 const Experience = ({ experience }) => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+	const experiences = experience.map((exp) => (
+		<td key={exp._id}>
+			<td>{exp.company}</td>
+			<td class='hide-sm'>{exp.title}</td>
+			<td>
+				{' '}
+				<Moment format='dd/mm/yyyy'>{exp.from}</Moment> -{' '}
+				{exp.to === null ? (
+					'Present'
+				) : (
+					<Moment format='dd/mm/yyyy'>{exp.to}</Moment>
+				)}
+			</td>
+		</td>
+	));
+
+	return (
+		<Fragment>
+			<h2 class='my-2'>Experience Credentials</h2>
+			<table class='table'>
+				<thead>
+					<tr>
+						<th>Company</th>
+						<th class='hide-sm'>Title</th>
+						<th class='hide-sm'>Years</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>{experiences}</tbody>
+			</table>
+		</Fragment>
+	);
+};
 
 Experience.propTypes = {
-    experience: propTypes.func.isRequired,
+	experience: propTypes.func.isRequired,
+};
 
-}
-
-export default connect( null)(Experience)
+export default connect(null)(Experience);
