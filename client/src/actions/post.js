799eb7e1d5c6b38aa2982ec1d6d8dd736c7deae6
manlikeghost <ekpoabasiama@gmail.com>
2021-12-17 13:58:44 +0100

    redux for updated likes set up

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 
-import { GET_POSTS, POST_ERROR } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
@@ -18,3 +18,18 @@ export const getPosts = () => async (dispatch) => {
 		});
 	}
 };
+// Update Likes 
+export const addLike = (id) => async ( dispatch ) => {
+	try {
+		const res = await axios.put( `/api/post/like/${ id }` );
+		dispatch( {
+			type: UPDATE_LIKES,
+			payload: res.data
+		})
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		});
+	}
+}
