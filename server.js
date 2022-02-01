c53b27b2d19e12438bdc7257fc145ddd784750ee
manlikeghost <ekpoabasiama@gmail.com>
2022-02-01 21:30:16 +0100

    setup for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require( 'express' );
 
 const connectDB = require( './config/db' );
+const path = require('path');
 
 const app = express();
 
@@ -21,6 +22,13 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/post', require('./routes/api/post'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+  app.get( '*', ( req, res ) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.listen(PORT, apiMessage);
